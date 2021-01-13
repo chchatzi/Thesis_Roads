f4dfb0b3f9167f3a5ca707578b13ed28787c6892
chchatzi <pioutrelos@gmail.com>
2021-01-13 18:05:46 +0200

    no message

---
+++ intersection_identify.py
@@ -0,0 +1,219 @@
+from geomet import wkt
+import json
+import math
+from shapely.geometry import LineString
+from shapely.geometry import Polygon
+from shapely.geometry import MultiLineString
+from shapely.geometry import Point
+
+
+def avgwidth(w):
+    s = 0
+    for i in w:
+        s += i[0]
+    avg = s / len(w)
+    return round(avg, 6)
+
+def avgwidth2(lst):
+    s = 0
+    for i in lst:
+        s += i
+    avg = s / len(lst)
+    return round(avg, 6)
+#checkforcross also checks if there is a T intersection 
+def checkforcross(twentylines, roadline):
+    '''this function takes the measuring lines that are not empty with their widths and checks for cross intersection
+    the main condition is the value of the mean of the values that located in the center of the line to be 2 times higher than the 2 other means
+    the one for the first 3 lines and the one of the last 3 lines. If that is the case then the road is tested if it is T intersection as well'''
+
+    print("this is the number of the actual widths: ", len(twentylines))
+    if len(twentylines) > 19:
+        first = [twentylines[0][0], twentylines[1][0], twentylines[2][0]]#, twentylines[3], twentylines[4], twentylines[5]]
+        mid = [twentylines[7][0], twentylines[8][0], twentylines[9][0], twentylines[10][0], twentylines[11][0], twentylines[12][0]]
+        last = [twentylines[17][0], twentylines[18][0], twentylines[19][0]]
+    elif len(twentylines) > 18:
+        first = [twentylines[0][0], twentylines[1][0], twentylines[2][0]] #, twentylines[3], twentylines[4], twentylines[5]]
+        mid = [twentylines[7][0], twentylines[8][0], twentylines[9][0], twentylines[10][0], twentylines[11][0], twentylines[12][0]]
+        last = [twentylines[16][0], twentylines[17][0], twentylines[18][0]]
+        
+    f = avgwidth2(first)
+    m = avgwidth2(mid)
+    l = avgwidth2(last)
+    print(f, "",m, "", l)
+    if m>f*2 and m>l*2:
+        #check if it is a T intersection case1
+        #take 4 measuring lines that are in the midlle
+        #take the 2 end points of each line
+        end8one = Point(twentylines[8][1].coords[0])
+        end8tow = Point(twentylines[8][1].coords[-1])
+        end9one = Point(twentylines[9][1].coords[0])
+        end9tow = Point(twentylines[9][1].coords[-1])
+        end10one = Point(twentylines[10][1].coords[0])
+        end10tow = Point(twentylines[10][1].coords[-1])
+        end11one = Point(twentylines[11][1].coords[0])
+        end11tow = Point(twentylines[11][1].coords[-1])
+
+        #find the midpoints of the road lines that are in line with the 4 mid lines
+        line9 = LineString([roadline.interpolate(0.4, normalized = True), roadline.interpolate(0.45, normalized = True)])
+        line10 = LineString([roadline.interpolate(0.45, normalized = True), roadline.interpolate(0.5, normalized = True)])
+        line11 = LineString([roadline.interpolate(0.5, normalized = True), roadline.interpolate(0.55, normalized = True)])
+        line12 = LineString([roadline.interpolate(0.55, normalized = True), roadline.interpolate(0.6, normalized = True)])
+        mid9 = line9.interpolate(0.5, normalized = True)
+        mid10 = line10.interpolate(0.5, normalized = True)
+        mid11 = line11.interpolate(0.5, normalized = True)
+        mid12 = line12.interpolate(0.5, normalized = True)
+        #calculate the distance between end point1 of a mid line and the corresponding point in the road line and 
+        # the distance between the other end point and the same point in road line
+        #if distance differ a lot append them to a list so we can check the len of the list after
+        differences = []
+        dif8start = LineString([end8one, mid9]).length
+        dif8end = LineString([end8tow, mid9]).length
+        if (dif8start-dif8end) > 0:
+            if (dif8start - dif8end) > 0.9*dif8end:
+                differences.append(1)
+        elif (dif8start-dif8end) < 0:
+            if dif8end-dif8start > 0.9*dif8start:
+                differences.append(1)
+
+        dif9start = LineString([end9one, mid10]).length
+        dif9end = LineString([end9tow, mid10]).length
+        if (dif9start-dif9end) > 0:
+            if (dif9start - dif9end) > 0.9*dif9end:
+                differences.append(1)
+        elif (dif9start-dif9end) < 0:
+            if dif9end-dif9start > 0.9*dif9start:
+                differences.append(1)
+        
+        dif10start = LineString([end10one, mid11]).length
+        dif10end = LineString([end10tow, mid11]).length
+        if (dif10start-dif10end) > 0:
+            if (dif10start - dif10end) > 0.9*dif10end:
+                differences.append(1)
+        elif (dif10start-dif10end) < 0:
+            if dif10end-dif10start > 0.9*dif10start:
+                differences.append(1)
+        
+        dif11start = LineString([end11one, mid12]).length
+        dif11end = LineString([end11tow, mid12]).length
+        if (dif11start-dif11end) > 0:
+            if (dif11start - dif11end) > 0.9*dif11end:
+                differences.append(1)
+        elif (dif11start-dif11end) < 0:
+            if dif11end-dif11start > 0.9*dif11start:
+                differences.append(1)
+
+        if len(differences) >= 3:
+            print("there is a T intersection")
+            return True
+        else:
+            print("there is a cross intersection")
+            return True 
+    else :
+        print("passes the test for cross intersection!")
+        return False
+
+#this function checks also for T intersection  BUT 
+def checkforTcase2(twentylines, roadline):
+    print("it passes the test for cross intersection i am checking for T intersection of case 2")
+    if len(twentylines) > 19:
+        first = [twentylines[0][0], twentylines[1][0], twentylines[2][0]]#, twentylines[3], twentylines[4], twentylines[5]]
+        mid = [twentylines[7][0], twentylines[8][0], twentylines[9][0], twentylines[10][0], twentylines[11][0], twentylines[12][0]]
+        last = [twentylines[17][0], twentylines[18][0], twentylines[19][0]]
+    elif len(twentylines) > 18:
+        first = [twentylines[0][0], twentylines[1][0], twentylines[2][0]] #, twentylines[3], twentylines[4], twentylines[5]]
+        mid = [twentylines[7][0], twentylines[8][0], twentylines[9][0], twentylines[10][0], twentylines[11][0], twentylines[12][0]]
+        last = [twentylines[16][0], twentylines[17][0], twentylines[18][0]]
+        
+    f = avgwidth2(first)
+    m = avgwidth2(mid)
+    l = avgwidth2(last)
+    print(f, "",m, "", l)
+    if m>f*1.5 and m>l*1.5:
+        #check if it is a T intersection
+        #take 4 measuring lines that are in the midlle
+        #take the 2 end points of each line
+        end8one = Point(twentylines[8][1].coords[0])
+        end8tow = Point(twentylines[8][1].coords[-1])
+        end9one = Point(twentylines[9][1].coords[0])
+        end9tow = Point(twentylines[9][1].coords[-1])
+        end10one = Point(twentylines[10][1].coords[0])
+        end10tow = Point(twentylines[10][1].coords[-1])
+        end11one = Point(twentylines[11][1].coords[0])
+        end11tow = Point(twentylines[11][1].coords[-1])
+
+        #find the midpoints of the road lines that are in line with the 4 mid lines
+        line9 = LineString([roadline.interpolate(0.4, normalized = True), roadline.interpolate(0.45, normalized = True)])
+        line10 = LineString([roadline.interpolate(0.45, normalized = True), roadline.interpolate(0.5, normalized = True)])
+        line11 = LineString([roadline.interpolate(0.5, normalized = True), roadline.interpolate(0.55, normalized = True)])
+        line12 = LineString([roadline.interpolate(0.55, normalized = True), roadline.interpolate(0.6, normalized = True)])
+        mid9 = line9.interpolate(0.5, normalized = True)
+        mid10 = line10.interpolate(0.5, normalized = True)
+        mid11 = line11.interpolate(0.5, normalized = True)
+        mid12 = line12.interpolate(0.5, normalized = True)
+        #calculate the distance between end point1 of a mid line and the corresponding point in the road line and 
+        # the distance between the other end point and the same point in road line
+        #if distance differ a lot append them to a list so we can check the len of the list after
+        differences = []
+        dif8start = LineString([end8one, mid9]).length
+        dif8end = LineString([end8tow, mid9]).length
+        if (dif8start-dif8end) > 0:
+            if (dif8start - dif8end) > 0.9*dif8end:
+                differences.append(1)
+        elif (dif8start-dif8end) < 0:
+            if dif8end-dif8start > 0.9*dif8start:
+                differences.append(1)
+
+        dif9start = LineString([end9one, mid10]).length
+        dif9end = LineString([end9tow, mid10]).length
+        if (dif9start-dif9end) > 0:
+            if (dif9start - dif9end) > 0.9*dif9end:
+                differences.append(1)
+        elif (dif9start-dif9end) < 0:
+            if dif9end-dif9start > 0.9*dif9start:
+                differences.append(1)
+        
+        dif10start = LineString([end10one, mid11]).length
+        dif10end = LineString([end10tow, mid11]).length
+        if (dif10start-dif10end) > 0:
+            if (dif10start - dif10end) > 0.9*dif10end:
+                differences.append(1)
+        elif (dif10start-dif10end) < 0:
+            if dif10end-dif10start > 0.9*dif10start:
+                differences.append(1)
+        
+        dif11start = LineString([end11one, mid12]).length
+        dif11end = LineString([end11tow, mid12]).length
+        if (dif11start-dif11end) > 0:
+            if (dif11start - dif11end) > 0.9*dif11end:
+                differences.append(1)
+        elif (dif11start-dif11end) < 0:
+            if dif11end-dif11start > 0.9*dif11start:
+                differences.append(1)
+
+        if len(differences) >= 3:
+            print("there is a T intersection case2")
+            return True
+        else:
+            print("the road is wider in the middle but is not T intersection or cross intersection")
+            return False
+    else :
+        return False
+
+def checkforTmaincase(twentylines, roadline):
+    print("it passed the test for T intersection of case 1 and 2, checking for T intersection Main case!")
+    if len(twentylines) > 19:
+        first = [twentylines[0][0], twentylines[1][0], twentylines[2][0], twentylines[3][0]]
+        last = [twentylines[17][0], twentylines[18][0], twentylines[19][0]]
+    elif len(twentylines) > 18:
+        first = [twentylines[0][0], twentylines[1][0], twentylines[2][0], twentylines[3][0]] 
+        last = [twentylines[16][0], twentylines[17][0], twentylines[18][0]]
+        
+    f = avgwidth2(first)
+    l = avgwidth2(last)
+    print("t main case first lines mean: ", f, "" "and last lines mean: ", l)
+    if  f > 3*l or l> 3*f:
+        print("Main case of T intersection")
+        return True
+    else:
+        return False
+
--- test_road_width.py
+++ test_road_width.py
@@ -10,7 +10,18 @@ from road_lines_polygons import linecross1case1,linecross1case2, polygoncross1
 from road_lines_polygons import linecross2case1, linecross2case2, polygoncross2
 from road_lines_polygons import linesmall1, linesmall2,polygonsmall
 from road_lines_polygons import toronto_intersection_cross_line, toronto_intersection_cross_polygon, toronto_intersection_t_line1, toronto_intersection_t_polygon
- 
+from road_lines_polygons import line_t_intersection_case1 , line_t_intersection_maincase, polygon_t_intersection_case1_and_maincase, line_t_intersection_case2, polygon_t_intersection_case2
+from intersection_identify import checkforcross, checkforTcase2, checkforTmaincase
+from road_lines_polygons import bigcenterline,polygonlist
+
+
+def fitlinetopolygons(line, polygonlist):
+    lines_polygons_pairs = []
+    for i in polygonlist:
+        fitline = line.intersection(i)
+        lines_polygons_pairs.append([fitline,i])
+    return lines_polygons_pairs
+
 
 def cutlinetotwenty(line):
     #change to 20line for more accuracy
@@ -84,10 +95,10 @@ def cutlinetoten(line):
 #function that takes a line and defines 2 offsets 
 #we did this in order to create a line prependicular to our road (the 10 lines that used to define the width of the road should be prependicular to the linear representation of the road)
 def makeoffsets(line):
-    leftline = line[0].parallel_offset(2, 'left')
-    rightline = line[0].parallel_offset(2, 'right')
-    print(leftline)
-    print(rightline)
+    leftline = line[0].parallel_offset(5, 'left')
+    rightline = line[0].parallel_offset(5, 'right')
+    #print(leftline)
+    #print(rightline)
     #print(leftline)
     #print(rightline)
     #we find the 2 midpoints of the 2 offset lines in order to connect them and create the prependicular line segment
@@ -116,7 +127,7 @@ def widt(prepedicularline, polygon, roadline):
                 ln = measureline
                 #print(measureline)
     else:
-        w = round(ln.length, 6)
+        w = round(ln.length, 4)
         #print(ln)
     return w, ln
 
@@ -134,183 +145,15 @@ def avgwidth2(lst):
     avg = s / len(lst)
     return round(avg, 6)
 
-def checkforcross(twentylines, roadline):
-    '''this function takes the measuring lines that are not empty with their widths and checks for cross intersection
-    the main condition is the value of the mean of the values that located in the center of the line to be 2 times higher than the 2 other means
-    the one for the first 3 lines and the one of the last 3 lines. If that is the case then the road is tested if it is T intersection as well'''
-
-    print("this is the number of the actual widths: ", len(twentylines))
-    if len(twentylines) > 19:
-        first = [twentylines[0][0], twentylines[1][0], twentylines[2][0]]#, twentylines[3], twentylines[4], twentylines[5]]
-        mid = [twentylines[7][0], twentylines[8][0], twentylines[9][0], twentylines[10][0], twentylines[11][0], twentylines[12][0]]
-        last = [twentylines[17][0], twentylines[18][0], twentylines[19][0]]
-    elif len(twentylines) > 18:
-        first = [twentylines[0][0], twentylines[1][0], twentylines[2][0]] #, twentylines[3], twentylines[4], twentylines[5]]
-        mid = [twentylines[7][0], twentylines[8][0], twentylines[9][0], twentylines[10][0], twentylines[11][0], twentylines[12][0]]
-        last = [twentylines[16][0], twentylines[17][0], twentylines[18][0]]
-        
-    f = avgwidth2(first)
-    m = avgwidth2(mid)
-    l = avgwidth2(last)
-    print(f, "",m, "", l)
-    if m>f*2 and m>l*2:
-        #check if it is a T intersection
-        #take 4 measuring lines that are in the midlle
-        #take the 2 end points of each line
-        end8one = Point(twentylines[8][1].coords[0])
-        end8tow = Point(twentylines[8][1].coords[-1])
-        end9one = Point(twentylines[9][1].coords[0])
-        end9tow = Point(twentylines[9][1].coords[-1])
-        end10one = Point(twentylines[10][1].coords[0])
-        end10tow = Point(twentylines[10][1].coords[-1])
-        end11one = Point(twentylines[11][1].coords[0])
-        end11tow = Point(twentylines[11][1].coords[-1])
-
-        #find the midpoints of the road lines that are in line with the 4 mid lines
-        line9 = LineString([roadline.interpolate(0.4, normalized = True), roadline.interpolate(0.45, normalized = True)])
-        line10 = LineString([roadline.interpolate(0.45, normalized = True), roadline.interpolate(0.5, normalized = True)])
-        line11 = LineString([roadline.interpolate(0.5, normalized = True), roadline.interpolate(0.55, normalized = True)])
-        line12 = LineString([roadline.interpolate(0.55, normalized = True), roadline.interpolate(0.6, normalized = True)])
-        mid9 = line9.interpolate(0.5, normalized = True)
-        mid10 = line10.interpolate(0.5, normalized = True)
-        mid11 = line11.interpolate(0.5, normalized = True)
-        mid12 = line12.interpolate(0.5, normalized = True)
-        #calculate the distance between end point1 of a mid line and the corresponding point in the road line and 
-        # the distance between the other end point and the same point in road line
-        #if distance differ a lot append them to a list so we can check the len of the list after
-        differences = []
-        dif8start = LineString([end8one, mid9]).length
-        dif8end = LineString([end8tow, mid9]).length
-        if (dif8start-dif8end) > 0:
-            if (dif8start - dif8end) > 0.9*dif8end:
-                differences.append(1)
-        elif (dif8start-dif8end) < 0:
-            if dif8end-dif8start > 0.9*dif8start:
-                differences.append(1)
-
-        dif9start = LineString([end9one, mid10]).length
-        dif9end = LineString([end9tow, mid10]).length
-        if (dif9start-dif9end) > 0:
-            if (dif9start - dif9end) > 0.9*dif9end:
-                differences.append(1)
-        elif (dif9start-dif9end) < 0:
-            if dif9end-dif9start > 0.9*dif9start:
-                differences.append(1)
-        
-        dif10start = LineString([end10one, mid11]).length
-        dif10end = LineString([end10tow, mid11]).length
-        if (dif10start-dif10end) > 0:
-            if (dif10start - dif10end) > 0.9*dif10end:
-                differences.append(1)
-        elif (dif10start-dif10end) < 0:
-            if dif10end-dif10start > 0.9*dif10start:
-                differences.append(1)
-        
-        dif11start = LineString([end11one, mid12]).length
-        dif11end = LineString([end11tow, mid12]).length
-        if (dif11start-dif11end) > 0:
-            if (dif11start - dif11end) > 0.9*dif11end:
-                differences.append(1)
-        elif (dif11start-dif11end) < 0:
-            if dif11end-dif11start > 0.9*dif11start:
-                differences.append(1)
-
-        if len(differences) >= 3:
-            print("there is a T intersection")
-            return True
-        else:
-            print("there is a cross intersection")
-            return True 
-    else :
-        return False
-
-def checkforTcase2(twentylines, roadline):
-    print("it passes the test for cross intersection i am checking for T intersection of case 2")
-    if len(twentylines) > 19:
-        first = [twentylines[0][0], twentylines[1][0], twentylines[2][0]]#, twentylines[3], twentylines[4], twentylines[5]]
-        mid = [twentylines[7][0], twentylines[8][0], twentylines[9][0], twentylines[10][0], twentylines[11][0], twentylines[12][0]]
-        last = [twentylines[17][0], twentylines[18][0], twentylines[19][0]]
-    elif len(twentylines) > 18:
-        first = [twentylines[0][0], twentylines[1][0], twentylines[2][0]] #, twentylines[3], twentylines[4], twentylines[5]]
-        mid = [twentylines[7][0], twentylines[8][0], twentylines[9][0], twentylines[10][0], twentylines[11][0], twentylines[12][0]]
-        last = [twentylines[16][0], twentylines[17][0], twentylines[18][0]]
-        
-    f = avgwidth2(first)
-    m = avgwidth2(mid)
-    l = avgwidth2(last)
-    print(f, "",m, "", l)
-    if m>f*1.6 and m>l*1.6:
-        #check if it is a T intersection
-        #take 4 measuring lines that are in the midlle
-        #take the 2 end points of each line
-        end8one = Point(twentylines[8][1].coords[0])
-        end8tow = Point(twentylines[8][1].coords[-1])
-        end9one = Point(twentylines[9][1].coords[0])
-        end9tow = Point(twentylines[9][1].coords[-1])
-        end10one = Point(twentylines[10][1].coords[0])
-        end10tow = Point(twentylines[10][1].coords[-1])
-        end11one = Point(twentylines[11][1].coords[0])
-        end11tow = Point(twentylines[11][1].coords[-1])
-
-        #find the midpoints of the road lines that are in line with the 4 mid lines
-        line9 = LineString([roadline.interpolate(0.4, normalized = True), roadline.interpolate(0.45, normalized = True)])
-        line10 = LineString([roadline.interpolate(0.45, normalized = True), roadline.interpolate(0.5, normalized = True)])
-        line11 = LineString([roadline.interpolate(0.5, normalized = True), roadline.interpolate(0.55, normalized = True)])
-        line12 = LineString([roadline.interpolate(0.55, normalized = True), roadline.interpolate(0.6, normalized = True)])
-        mid9 = line9.interpolate(0.5, normalized = True)
-        mid10 = line10.interpolate(0.5, normalized = True)
-        mid11 = line11.interpolate(0.5, normalized = True)
-        mid12 = line12.interpolate(0.5, normalized = True)
-        #calculate the distance between end point1 of a mid line and the corresponding point in the road line and 
-        # the distance between the other end point and the same point in road line
-        #if distance differ a lot append them to a list so we can check the len of the list after
-        differences = []
-        dif8start = LineString([end8one, mid9]).length
-        dif8end = LineString([end8tow, mid9]).length
-        if (dif8start-dif8end) > 0:
-            if (dif8start - dif8end) > 0.9*dif8end:
-                differences.append(1)
-        elif (dif8start-dif8end) < 0:
-            if dif8end-dif8start > 0.9*dif8start:
-                differences.append(1)
-
-        dif9start = LineString([end9one, mid10]).length
-        dif9end = LineString([end9tow, mid10]).length
-        if (dif9start-dif9end) > 0:
-            if (dif9start - dif9end) > 0.9*dif9end:
-                differences.append(1)
-        elif (dif9start-dif9end) < 0:
-            if dif9end-dif9start > 0.9*dif9start:
-                differences.append(1)
-        
-        dif10start = LineString([end10one, mid11]).length
-        dif10end = LineString([end10tow, mid11]).length
-        if (dif10start-dif10end) > 0:
-            if (dif10start - dif10end) > 0.9*dif10end:
-                differences.append(1)
-        elif (dif10start-dif10end) < 0:
-            if dif10end-dif10start > 0.9*dif10start:
-                differences.append(1)
-        
-        dif11start = LineString([end11one, mid12]).length
-        dif11end = LineString([end11tow, mid12]).length
-        if (dif11start-dif11end) > 0:
-            if (dif11start - dif11end) > 0.9*dif11end:
-                differences.append(1)
-        elif (dif11start-dif11end) < 0:
-            if dif11end-dif11start > 0.9*dif11start:
-                differences.append(1)
-
-        if len(differences) >= 3:
-            print("there is a T intersection case2")
-            return True
-        else:
-            print("the road is wider in the middle but is not T intersection or cross intersection")
-            return False
-    else :
-        return False
-
-#def checkforTmaincase 
+def mean_w_centerline(widthsofcenterline):
+    for i in widthsofcenterline:
+        if i == 0:
+            widthsofcenterline.remove(i)
+    s = 0
+    for ii in widthsofcenterline:
+        s += ii
+    mean = s / len(widthsofcenterline)
+    return mean
 
 def widthcalculation(line, polygon):
     tenlines = cutlinetotwenty(line)
@@ -323,14 +166,15 @@ def widthcalculation(line, polygon):
         width = widt(prep_line[0], polygon, i[0])[0]
         measuringline = widt(prep_line[0], polygon, i[0])[1]
         w.append([width,prep_line[1],measuringline])
-    #for iss in allpreplines:
-        #print("opaaa", iss)
+    # w list contains --> [width value, number of measuring line, measuring line wkt]
     for isss in w:
         print("this is width of measuring line: ", isss)
         if isss[0] == 0.0:
             w.remove(isss)
             print("i found an empty line:", isss)
     #make a list only with the 20 widhts to use it for checking if there is a cross intersection
+
+    # onlywidths contains --> [width value, measuring line wkt]
     onlywidths = []
     for wdt in w:
         onlywidths.append([wdt[0], wdt[2]])
@@ -351,7 +195,10 @@ def widthcalculation(line, polygon):
         return 0
     elif checkforTcase2(onlywidths, line):
         return 0
+    elif checkforTmaincase(onlywidths, line):
+        return 0
     else:
+        #outliers contains --> [number of measuring line, width, 1 or 0]
         for iii in w:
             if iii[0] > m + 1.5*std: 
                 outliers.append([iii[1], iii[0], 1])
@@ -364,7 +211,7 @@ def widthcalculation(line, polygon):
                     print("i removed", out[0])
                     w.remove(widths)
         #find the lines that are marked as outliers
-        outlier_linestrings = []
+        outlier_linestrings = [] # --> []
         outliers2 = [] # a list that contains linestring, id, width of outlier and 0 if lower from mean 1 if higher of mean
         for ln in allpreplines:
             for outl in outliers:
@@ -381,9 +228,25 @@ def widthcalculation(line, polygon):
         n = round(avgwidth(w), 5)
         return n
 
-    
+#fit the big centerline of the road to each polygon that intersects   
+lines_polygons_pairs = fitlinetopolygons(bigcenterline, polygonlist)
+widthsofcenterline = []
+#estimate the width for each pair of centerline-polygon
+for i in lines_polygons_pairs:
+    n = widthcalculation(i[0], i[1])
+    widthsofcenterline.append(n)
+#show the width of each polygon
+num_of_width = 1   
+for i in widthsofcenterline:
+    print("width of ", num_of_width, " polygon is: ", i)
+    num_of_width += 1
 
-n = widthcalculation(line1, polygon1)
-print("final width is: ", n)
+#compute a mean width for the whole centerline
+final_w_road = mean_w_centerline(widthsofcenterline)
+print("this is the final width of the road: ", final_w_road)
+
+
+
+
 
 
